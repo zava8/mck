4b6a0e72edb72ffc6f41fa10eb381096de0c57c6
viml <vimalkg@gmail.com>
2024-02-28 18:39:12 +0530

    TylerPottsDev mck app

--- src/components/mck.vue
+++ src/components/mck.vue
@@ -1,10 +1,33 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
-</script>
-
-<style></style>
+<template>
+    <div>
+      <h3>{{ question.text }}</h3>
+      <ul>
+        <li v-for="answer in question.answers" :key="answer.id">
+          <input type="radio" v-model="selectedAnswer" :value="answer.id" />
+          {{ answer.text }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "Question",
+    props: {
+      question: {
+        type: Object,
+        required: true
+      }
+    },
+    data() {
+      return {
+        selectedAnswer: null
+      };
+    },
+    watch: {
+      selectedAnswer(value) {
+        this.$emit("answer-selected", value);
+      }
+    }
+  };
+  </script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import FoodItem from './components/FoodItem.vue'
-// createApp(App).mount('#app')
-const app = createApp(App)
-app.component('food-item', FoodItem)
-app.mount('#app')
+// import FoodItem from './components/FoodItem.vue'
+createApp(App).mount('#app')
+//const app = createApp(App)
+//app.component('food-item', FoodItem)
+//app.mount('#app')
+
+
+// https://github.com/TylerPottsDev/yt-vue-quiz/blob/master/index.html
+
